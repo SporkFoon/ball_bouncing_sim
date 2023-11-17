538e201267c4038e33ba504d12158349b9f1d56b
Navin Bunthuphanich <navin.b@ku.th>
2023-11-17 23:32:30 +0700

    Rewrite the code into OOP-style (Prototype version)

--- run_ball.py
+++ run_ball.py
@@ -1,27 +1,36 @@
 import turtle
 import ball
 
-num_balls = int(input("Number of balls to simulate: "))
-turtle.speed(0)
-turtle.tracer(0)
-turtle.hideturtle()
-canvas_width = turtle.screensize()[0]
-canvas_height = turtle.screensize()[1]
-ball_radius = 0.05 * canvas_width
-turtle.colormode(255)
-color_list = []
-xpos = []
-ypos = []
-vx = []
-vy = []
-ball_color = []
-ball.initilizing(xpos, ypos, vx, vy, ball_color, canvas_width, canvas_height, ball_radius, num_balls)
-while (True):
-    turtle.clear()
-    for i in range(num_balls):
-        ball.draw_circle(ball_color[i], ball_radius, xpos[i], ypos[i])
-        ball.move_circle(i, xpos, ypos, vx, vy, canvas_width, canvas_height, ball_radius)
-    turtle.update()
+class BallSimulation:
+    def __init__(self, num_balls):
+        turtle.speed(0)
+        turtle.tracer(0)
+        turtle.hideturtle()
+        self.canvas_width = turtle.screensize()[0]
+        self.canvas_height = turtle.screensize()[1]
+        self.ball_radius = 0.05 * self.canvas_width
+        turtle.colormode(255)
+        self.balls = self.initialize_balls(num_balls)
 
-# hold the window; close it by clicking the window close 'x' mark
+    def initialize_balls(self, num_balls):
+        balls = []
+        for _ in range(num_balls):
+            ball_obj = ball.Ball(self.canvas_width, self.canvas_height, self.ball_radius)
+            balls.append(ball_obj)
+        return balls
+
+    def run_simulation(self):
+        while True:
+            turtle.clear()
+            for ball_obj in self.balls:
+                ball_obj.draw()
+                ball_obj.move(self.canvas_width, self.canvas_height)
+            turtle.update()
+
+
+num_balls = int(input("Number of balls to simulate: "))
+simulation = BallSimulation(num_balls)
+simulation.run_simulation()
+
+# Hold the window; close it by clicking the window close 'x' mark
 turtle.done()
